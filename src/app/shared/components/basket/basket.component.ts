eb43ea176cf21b19b3e44346ebcc42aaa245a299
Esra Özdoğan <50195220+EsraOzdogan@users.noreply.github.com>
2022-09-11 17:43:02 +0300

    Update basket.component.ts

--- src/app/shared/components/basket/basket.component.ts
+++ src/app/shared/components/basket/basket.component.ts
@@ -8,8 +8,12 @@ import { OfferService } from 'src/app/features/offers/services/offer/offer.servi
   styleUrls: ['./basket.component.css'],
 })
 export class BasketComponent implements OnInit {
-  basket!: Offer[];
-
+  basket: Offer[] = [];
+  selectedCatalogOfferList!: Offer[];
+  selectedCampaignOffersList!: Offer[];
+  campaignOffersList!: Offer[];
+  matchCampaignList: Offer[] = [];
+  findMatch!: Offer;
   constructor(private offerService: OfferService) {}
 
   ngOnInit(): void {
@@ -17,8 +21,65 @@ export class BasketComponent implements OnInit {
   }
   getBasket() {
     this.offerService.basket$.subscribe((data) => {
-      this.basket = data;
+      //this.basket = data;
+      //console.warn(this.basket);
+
+      this.selectedCatalogOfferList = data.filter((offer) => {
+        return offer.type.typeName == 'catalog';
+      });
+
+      this.selectedCampaignOffersList = data.filter((offer) => {
+        return offer.type.typeName == 'campaign';
+      });
+
+      this.offerService.getList().subscribe((data) => {
+        this.campaignOffersList = data.filter(
+          (offer) => offer.type.typeName === 'campaign'
+        );
+
+        //console.log('bbbbbbb', this.campaignOffersList);
+        //console.log('selected', this.selectedCampaignOffersList);
+
+        this.campaignOffersList.forEach((campaign) => {
+          this.selectedCampaignOffersList.forEach((selectedCampaign) => {
+            if (campaign.type.id === selectedCampaign.type.id) {
+              //console.warn('match', campaign);
+              this.matchCampaignList.push(campaign);
+            } else {
+              //console.warn('notmatch', campaign);
+            }
+          });
+          //console.warn('aaaaaaa', this.matchCampaignList);
+        });
+        //this.basket = this.selectedCatalogOfferList;
+
+        //this.matchCampaignList.push(this.findMatch);
+        //console.warn(this.matchCampaignList);
+
+        // this.campaignOffersList = data.filter(
+        //   (offer) => offer.type.typeName === 'campaign'
+        // );
+      });
+
+      // this.matchCampaignList.forEach((campaign) => {
+      //   console.log('aaaaassssssaaaa', campaign);
+      //   //this.selectedCatalogOfferList.push(campaign);
+      // });
+      //console.log('basket', this.selectedCatalogOfferList);
+
+      //console.log('catalog', this.selectedCatalogOfferList);
+      console.log('matchArray', this.matchCampaignList);
+      console.log('catalog', this.selectedCatalogOfferList);
+
+      // this.matchCampaignList.forEach((campaign) => {
+      //   this.basket.push(campaign);
+      // });
+      // this.selectedCatalogOfferList.forEach((catalog) => {
+      //   this.basket.push(catalog);
+      // });
+      console.log('basket', this.basket);
     });
+    //console.warn('matchArrayaaaaa', this.matchCampaignList);
   }
   get amount(): number {
     let sumAmount: number = 0;

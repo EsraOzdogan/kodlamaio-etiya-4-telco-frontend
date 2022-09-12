8960c9c57aa38f43a5154c6e8e769cc029fc51b1
Esra Özdoğan <50195220+EsraOzdogan@users.noreply.github.com>
2022-09-13 00:29:40 +0300

    refactor table-accordion

--- src/app/shared/components/table-accordion/table-accordion.component.ts
+++ src/app/shared/components/table-accordion/table-accordion.component.ts
@@ -1,3 +1,4 @@
+import { Offer } from './../../../features/offers/models/offer';
 import { Product } from 'src/app/features/customers/models/product';
 import { Customer } from 'src/app/features/customers/models/customer';
 import { Router } from '@angular/router';
@@ -69,6 +70,60 @@ export class TableAccordionComponent implements OnInit {
         .subscribe((data) => {
           this.customer = data;
         });
+    }
+  }
+
+  productDetail(billingAccount: BillingAccount, offer: Offer) {
+    if (offer.type.typeName == 'campaign') {
+      let campaignProdOfferId = offer.id.toString();
+      let campaignProdOfferName = offer.name;
+      let campaignId = offer.type.id.toString();
+      let campaignAddressDetail: any = [];
+      billingAccount.addresses.forEach(
+        (data) => (campaignAddressDetail = data)
+      );
+      this.messageService.add({
+        key: 'product-detail',
+        sticky: true,
+        severity: 'warn',
+        detail:
+          'Prod Offer Id: ' +
+          campaignProdOfferId +
+          '       ' +
+          'Prod Offer Name: ' +
+          campaignProdOfferName +
+          '       ' +
+          'Campaign Id: ' +
+          campaignId +
+          '       ' +
+          campaignAddressDetail.city.name +
+          '       ' +
+          campaignAddressDetail.description +
+          '       ',
+      });
+    } else if (offer.type.typeName == 'catalog') {
+      let catalogProdOfferId = offer.id;
+      let catalogProdOfferName = offer.name;
+      let catalogAddressDetail: any = [];
+      billingAccount.addresses.forEach((data) => (catalogAddressDetail = data));
+      this.messageService.add({
+        key: 'product-detail',
+        sticky: true,
+        severity: 'warn',
+        detail:
+          'ProdOfferId: ' +
+          catalogProdOfferId +
+          '         ' +
+          'ProdOfferName: ' +
+          catalogProdOfferName +
+          '          ' +
+          'Address Name: ' +
+          catalogAddressDetail.city.name +
+          '          ' +
+          'Address Description: ' +
+          catalogAddressDetail.description +
+          '          ',
+      });
     }
   }
   removePopup(billingAccount: BillingAccount) {
